283a424313bfd590d71ee95436cc6fc338a66a4b
Juang Maulana Taruna Putra <117959461+juangmaulana@users.noreply.github.com>
2025-10-23 17:55:48 +0700

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -11,7 +11,7 @@
  <header id="header" class="fixed-top">
      <div class="container d-flex align-items-center">
 
-         <h1 class="logo me-auto"><a href="/">RS Kebumen 2</a></h1>
+         <h1 class="logo me-auto"><a href="/">RS AMBARawa</a></h1>
 
          <nav id="navbar" class="navbar order-last order-lg-0">
              <ul>
